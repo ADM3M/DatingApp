a64eac0b3a4bf7c91151c997a028f5bb220bf295
ADM3M <fly.swift499@gmail.com>
2022-04-29 17:06:19 +0300

    Added main photo select (client)

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -45,4 +45,8 @@ export class MemberService {
       })
     );
   }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + "users/set-main-photo/" + photoId, {})
+  }
 }
---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
+import { IMember } from 'src/app/_models/IMember';
+import { Photo } from 'src/app/_models/IPhoto';
+import { IUser } from 'src/app/_models/IUser';
+import { AccountService } from 'src/app/_services/account.service';
+import { MemberService } from 'src/app/_services/member.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+
+  @Input() member: IMember;
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: IUser;
+
+  constructor(private accountService: AccountService, private membersService: MemberService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + "users/add-photo",
+      authToken: "Bearer " + this.user.token,
+      isHTML5: true,
+      allowedFileType: ["image"],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.membersService.setMainPhoto(photo.id).subscribe(() => {
+      this.user.photoUrl = photo.url;
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+      this.member.photos.forEach(p => {
+        if (p.isMain) p.isMain = false;
+        if (p.id === photo.id) p.isMain = true;
+      })
+    })
+  }
+}
