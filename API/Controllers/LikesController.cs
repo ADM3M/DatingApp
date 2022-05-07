2c9fab1c40b685eecbe7450f93c2bc28859a98fa
ADM3M <fly.swift499@gmail.com>
2022-05-07 21:21:11 +0300

    Added and configured Like controller.

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTO;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _likesRepository = likesRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string userName)
+        {
+            var sourceUser = await _likesRepository.GetUserWithLikes(User.GetUserId());
+            
+            if (sourceUser.Name == userName) return BadRequest("You can't like yourself");
+            
+            var likedUser = await _userRepository.GetUserByUserNameAsync(userName);
+            if (likedUser is null) return NotFound();
+
+            var userLike = await _likesRepository.GetUserLike(sourceUser.Id, likedUser.Id);
+
+            if (userLike is not null)
+            {
+                sourceUser.LikedUsers.Remove(userLike);
+                return await _userRepository.SaveAllAsync() ? Ok("Like has been deleted")
+                : BadRequest("Error while deleting user like");
+            }
+
+            userLike = new Entities.UserLike
+            {
+                SourceUserId = sourceUser.Id,
+                LikedUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            if (await _userRepository.SaveAllAsync()) return Ok("User has been liked");
+
+            return BadRequest("Failed to like user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDTO>>> GetUserLikes(string predicate)
+        {
+            var userLikes = await _likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(userLikes);
+        }
+
+    }
+}
