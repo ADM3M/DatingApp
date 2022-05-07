71d56cd07016c09a0956252aec0f634a522ee1ef
ADM3M <fly.swift499@gmail.com>
2022-05-07 23:52:51 +0300

    Created and implemented MessageRepository.

---
+++ API/DTO/MessageDTO.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }
+
+        public string SenderName { get; set; }
+
+        public string SenderPhotoUrl { get; set; }
+
+        public int RecipientId { get; set; }
+
+        public string RecipientName { get; set; }
+
+        public string RecipientPhotoUrl { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime? DateRead { get; set; }
+
+        public DateTime MessageSent { get; set; }
+    }
+}
--- API/Extensions/ApplicationSeviceExtensions.cs
+++ API/Extensions/ApplicationSeviceExtensions.cs
@@ -21,6 +21,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
