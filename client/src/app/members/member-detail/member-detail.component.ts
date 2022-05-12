06243f4386ac2dae95c1fd01d3a4c6e5af7a5d0f
ADM3M <fly.swift499@gmail.com>
2022-05-12 11:57:17 +0300

    Implemented message notifier (client).

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,6 +1,6 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { ToastrService } from 'ngx-toastr';
@@ -31,10 +31,14 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private messageService: MessageService,
     private toastr: ToastrService,
-    private accountService: AccountService) {
+    private accountService: AccountService,
+    private router: Router)
+     {
       this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
         this.user = user;
       });
+
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
   ngOnInit(): void {
     this.route.data.subscribe(data => {
